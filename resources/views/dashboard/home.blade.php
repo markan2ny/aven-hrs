9d1958098ddc4dbbfee03344086b3c514912556f
unknown <admin@NMS-Server.avendomain.local>
2021-12-13 09:14:56 +0800

    Update table

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -12,10 +12,9 @@
                     <table class="table">
                         <thead>
                           <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
+                            <th scope="col">Emp#</th>
+                            <th scope="col">Acct#</th>
+                            <th scope="col">Name</th>
                           </tr>
                         </thead>
                         <tbody>
@@ -23,18 +22,15 @@
                             <th scope="row">1</th>
                             <td>Mark</td>
                             <td>Otto</td>
-                            <td>@mdo</td>
                           </tr>
                           <tr>
                             <th scope="row">2</th>
                             <td>Jacob</td>
                             <td>Thornton</td>
-                            <td>@fat</td>
                           </tr>
                           <tr>
                             <th scope="row">3</th>
                             <td colspan="2">Larry the Bird</td>
-                            <td>@twitter</td>
                           </tr>
                         </tbody>
                       </table>
